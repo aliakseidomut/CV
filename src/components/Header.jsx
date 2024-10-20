97ce24611a1325c889974d83cfa162baf658d903
Aliaksei Domut <alexedomut@gmail.com>
2024-10-21 01:46:00 +0300

    e

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ export default function Header() {
       <div className="flex gap-3 justify-center">
         <span>Minsk, Belarus</span>
         <a className="underline" href="mailto:alexeydomut@gmail.com">alexeydomut@gmail.com</a>
+        <a className="underline" href="https://github.com/aliakseidomut">GitHub</a>
       </div>
       <p className="text-lg font-normal">
         Hi, I am a motivated student and aspiring frontend developer with foundational experience in HTML, CSS, JavaScript, and React, along with some practical experience with Node.js. I am proficient in building responsive web pages and user interfaces, with a growing interest in backend development. I have completed courses at RSSchool and continually expand my skills through personal and university projects. I am familiar with Git for version control and eager to learn new technologies, contributing to both frontend and backend development. I am looking for opportunities to apply my knowledge and grow as a full-stack developer.
