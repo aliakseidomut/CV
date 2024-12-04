e465196cafe1e629b105cd136b0225e2dd2d4f47
Aliaksei Domut <alexedomut@gmail.com>
2024-12-04 03:26:06 +0300

    add art museum in projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,56 @@
-import Header from './components/Header'
-import InfoList from './components/InfoList'
-import Projects from './components/Projects'
+import Header from "./components/Header";
+import InfoList from "./components/InfoList";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="w-2/4 mx-auto">
       <Header />
-      
-      <InfoList 
-        name = {"Skills"}
-        items = {["JavaScript", "HTML", "CSS", "React", "Redux", "TypeScript", "NodeJS:Beginner"]}
-      />
-      
-      <InfoList 
-        name = {"Education"}
-        items = {["Belarusian State University", 
-                  "Faculty of mechanics and mathematics",
-                  "Bachelors program: Mathematics and Computer Science (Web Programming and Internet Technologies)",
-                  "2022-2026"
-                ]}
+
+      <InfoList
+        name={"Skills"}
+        items={[
+          "JavaScript",
+          "HTML",
+          "CSS",
+          "React",
+          "Redux",
+          "TypeScript",
+          "NodeJS:Beginner",
+        ]}
       />
 
-      <InfoList 
-        name = {"Experience"}
-        items = {['Developed numerous educational projects in JavaScript and TypeScript as part of the "JavaScript/Front-end" course at RS School.', 
-                  'Developed various educational projects using JavaScript and React as part of my university studies.',
-                  'Created a clone of the card game "Gwent" using React, TypeScript, Redux, ExpressJS, and MongoDB as a coursework project during my second year at university (the project was partially completed).'
-                ]}
+      <InfoList
+        name={"Education"}
+        items={[
+          "Belarusian State University",
+          "Faculty of mechanics and mathematics",
+          "Bachelors program: Mathematics and Computer Science (Web Programming and Internet Technologies)",
+          "2022-2026",
+        ]}
       />
 
-      <InfoList 
-        name = {"Languages"}
-        items = {["English: Elementary", "Belarusian: Native", "Russian: Native"]}
+      <InfoList
+        name={"Experience"}
+        items={[
+          'Developed numerous projects in JavaScript and TypeScript as part of the "JavaScript/Front-end" course at RS School.',
+          "Developed various projects using JavaScript and React as part of my university studies.",
+          'Created a clone of the card game "Gwent" using React, TypeScript, Redux, ExpressJS, and MongoDB as a coursework project during my second year at university (the project was partially completed).',
+        ]}
       />
-      
+
+      <InfoList
+        name={"Languages"}
+        items={[
+          "English: Intermediate",
+          "Belarusian: Native",
+          "Russian: Native",
+        ]}
+      />
+
       <Projects />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,42 @@
-import ProjectsItem from "./ProjectsItem"
+import ProjectsItem from "./ProjectsItem";
 
 export default function Projects() {
   const projects = [
     {
-      title: 'ToDo',
-      gitHubUrl: 'https://github.com/aliakseidomut/ToDo',
-      id: '1'
+      title: "ToDo",
+      gitHubUrl: "https://github.com/aliakseidomut/ToDo",
+      id: "1",
     },
     {
-      title: 'Notions',
-      gitHubUrl: 'https://github.com/AliakseiDonut/Notion',
-      id: '2'
+      title: "Notions",
+      gitHubUrl: "https://github.com/AliakseiDonut/Notion",
+      id: "2",
     },
     {
-      title: 'Store',
-      gitHubUrl: 'https://github.com/AliakseiDonut/store',
-      id: '3'
+      title: "Store",
+      gitHubUrl: "https://github.com/AliakseiDonut/store",
+      id: "3",
     },
     {
-      title: 'GwentClone',
-      gitHubUrl: 'https://github.com/aliakseidomut/GwentClone',
-      id: '4'
-    }
-  ]
-  
+      title: "GwentClone",
+      gitHubUrl: "https://github.com/aliakseidomut/GwentClone",
+      id: "4",
+    },
+    {
+      title: "Art Museum",
+      gitHubUrl: "https://github.com/aliakseidomut/modsen-art-museum4-main",
+      id: "5",
+    },
+  ];
+
   return (
     <div className="py-4">
       <h2 className="text-3xl mb-2">Projects</h2>
       <div className="flex py-5 gap-10">
-        {projects.map(el => <ProjectsItem key={el.id} item={ el } />)}
+        {projects.map((el) => (
+          <ProjectsItem key={el.id} item={el} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
